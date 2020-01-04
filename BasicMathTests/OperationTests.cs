a6295c8411df9896d5734ff91f6e8c2d7e2b2ede
Zodd <paolo.papanikolaou@outlook.com>
2020-01-04 22:07:20 +0100

    Commit Test

---
+++ BasicMathTests/OperationTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZoddProductions.ZoddLibraries.BasicMath;
+
+namespace BasicMathTests
+{
+    [TestFixture]
+    public class OperationTests
+    {
+        [TestCase(1, 1, ExpectedResult = 2)]
+        [TestCase(1, 2, ExpectedResult = 3)]
+        [TestCase(1, -1, ExpectedResult = 0)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(-5, -5, ExpectedResult = -10)]
+        public int AdditionTests(int value1, int value2)
+        {
+            return Operation.Addition(value1, value2);
+        }
+    }
+}
